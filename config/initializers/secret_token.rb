7ff522ba58e6f028be90ae2d45b89b27626902e0
treby <treby@atelier-nodoka.net>
2014-04-07 01:49:19 +0900

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Otonoki::Application.config.secret_key_base = '0d4fd7f52f641f2c1104935857bef54434bc144def3e877bb2a7bf4cb3b55557ef40570fcda235f006e7f1d144539b7ba130ebcec1a35bc239c3090e25c34981'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Otonoki::Application.config.session_store :cookie_store, key: '_otonoki_session'
